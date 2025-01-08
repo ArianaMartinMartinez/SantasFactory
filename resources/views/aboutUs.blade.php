af851330f60964d69e7429456556b9cf8cc87c2e
Jaz356 <joziahwalters70@gmail.com>
2025-01-08 12:46:56 +0100

    feat: add about us structure and First sprint images

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -1,3 +1,34 @@
+<h2>
+   About Us
+This project was developed by two different teams
+</h2>
+
 <div>
- 
+    <h3>
+        First Sprint.
+    </h3>
+    <div>
+        <img src="https://avatars.githubusercontent.com/u/117454655?v=4" alt="">
+        <p></p>
+    </div>
+    <div>
+        <img src="https://avatars.githubusercontent.com/u/185358023?v=4" alt="">
+        <p></p>
+    </div> 
+    <div>
+        <img src="https://avatars.githubusercontent.com/u/145048993?v=4" alt="">
+        <p></p>
+    </div> 
+    <div>
+        <img src="https://avatars.githubusercontent.com/u/135219405?v=4" alt="">
+        <p></p>
+    </div> 
+    <div>
+        <img src="https://avatars.githubusercontent.com/u/185358222?v=4" alt="">
+        <p></p>
+    </div> 
+    <div>
+        <img src="https://avatars.githubusercontent.com/u/185357841?v=4" alt="">
+        <p></p>
+    </div>
 </div>
