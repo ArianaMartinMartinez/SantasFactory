e68a1be4c453bb75bbffd67668a54657ba3e330a
angiehelensanchez <angiehelensanchez@gmail.com>
2025-01-08 14:41:53 +0100

    fix: banner style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
 @section('content')
         <section class="hero">
-            <h2>Discover the Magic of Christmas!</h2>
-            <button class="cta-btn"><a class="linkButton" href="shop.html">Shop Now!!</a></button>
+            <h2>HO HO HO!! Welcome To Santa's Toy Factory</h2>
         </section>
 
-        <section class="products" id="products">
+        <section class="products">
             <h3>Our Top Toys</h3>
             <div class="product-grid">
                 <div class="product-card">Toy 1</div>
