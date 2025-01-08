3e1f2bc16dccd5a52f308d64d559ede18402c4d3
angiehelensanchez <angiehelensanchez@gmail.com>
2025-01-08 12:47:05 +0100

    fix: footer style

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,26 +1,4 @@
 <footer>
     <p>© 2024 Santa's Toy Factory | HO HO HO! 🎅</p>
-    <a href="https://github.com/sr-calcetines" target="_blank" class="git">
-        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
-    </a>
-
-    <a href="https://github.com/rebecabernal" target="_blank" class="git">
-        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
-    </a>
-
-    <a href="https://github.com/rodrigoo1604" target="_blank" class="git">
-        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
-    </a>
-
-    <a href="https://github.com/DinGo21" target="_blank" class="git">
-        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
-    </a>
-
-    <a href="https://github.com/ugotchriscoded" target="_blank" class="git">
-        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
-    </a>
-
-    <a href="https://github.com/talenaa" target="_blank" class="git">
-        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
-    </a>
+    <p>About us</p>
 </footer>
