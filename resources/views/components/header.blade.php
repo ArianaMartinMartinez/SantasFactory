842897a037e951ca94e3212694bde3251a9ab536
angiehelensanchez <angiehelensanchez@gmail.com>
2025-01-08 14:41:06 +0100

    fix: header links

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <h1>SANTA'S FACTORY</h1>
     <div class="headerButtons">
-        <a href=""><img src="{{ asset('img/buttons/buttonSanta.png')}}" alt=""></a>
-        <a href=""><img src="{{asset('img/buttons/buttonElf.png')}}" alt=""></a>
+        <a href="{{ route('kidshome') }}"><img src="{{ asset('img/buttons/buttonSanta.png')}}" alt=""></a>
+        <a href="{{ route('toyshome') }}"><img src="{{asset('img/buttons/buttonElf.png')}}" alt=""></a>
     </div>
 </header>
