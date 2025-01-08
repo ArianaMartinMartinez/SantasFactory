dcd4453e749a5df62982950d9e9ec5e1362feb84
Jaz356 <joziahwalters70@gmail.com>
2025-01-08 13:14:33 +0100

    feat: add alt about us

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -8,27 +8,27 @@ This project was developed by two different teams
         First Sprint.
     </h3>
     <div>
-        <img src="https://avatars.githubusercontent.com/u/117454655?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/117454655?v=4" alt="Pepe Image">
         <a>Pepe</a>
     </div>
     <div>
-        <img src="https://avatars.githubusercontent.com/u/185358023?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/185358023?v=4" alt="Rebecca Image">
         <a>Rebecca</a>
     </div> 
     <div>
-        <img src="https://avatars.githubusercontent.com/u/145048993?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/145048993?v=4" alt="Rodrigo Image">
         <a>Rodrigo</a>
     </div> 
     <div>
-        <img src="https://avatars.githubusercontent.com/u/135219405?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/135219405?v=4" alt="Diego Image">
         <a>Diego</a>
     </div> 
     <div>
-        <img src="https://avatars.githubusercontent.com/u/185358222?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/185358222?v=4" alt="Valeria Image">
         <a>Valeria</a>
     </div> 
     <div>
-        <img src="https://avatars.githubusercontent.com/u/185357841?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/185357841?v=4" alt="Talena Image">
         <a>Talena</a>
     </div>
 </div>
@@ -38,27 +38,27 @@ This project was developed by two different teams
         Second Sprint.
     </h3>
     <div>
-        <img src="https://avatars.githubusercontent.com/u/63065122?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/63065122?v=4" alt="Angie Image">
         <a>Angie</a>
     </div>
     <div>
-        <img src="https://avatars.githubusercontent.com/u/104553851?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/104553851?v=4" alt="Ariana Image">
         <a>Ariana</a>
     </div> 
     <div>
-        <img src="https://avatars.githubusercontent.com/u/185358197?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/185358197?v=4" alt="Alberto Image">
         <a>Alberto</a>
     </div> 
     <div>
-        <img src="https://avatars.githubusercontent.com/u/185357978?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/185357978?v=4" alt="Jaz Image">
         <a>Jaz</a>
     </div> 
     <div>
-        <img src="https://avatars.githubusercontent.com/u/128361015?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/128361015?v=4" alt="Inma Image">
         <a>Inma</a>
     </div> 
     <div>
-        <img src="https://avatars.githubusercontent.com/u/118169171?v=4" alt="">
+        <img src="https://avatars.githubusercontent.com/u/118169171?v=4" alt="Alejandro Image">
         <a>Alejandro</a>
     </div>
 </div>
