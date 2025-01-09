17710b74192985d64b860b372041d3c0a3b7d9a2
angiehelensanchez <angiehelensanchez@gmail.com>
2025-01-09 12:03:57 +0100

    feat: add sttistics section and fix top toys style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,37 @@
             <h2>HO HO HO!! Welcome To Santa's Toy Factory</h2>
         </section>
 
+        <section class="statistics">
+            <div class="containerData">
+                <img src="{{asset('img/buttons/faceSanta.png')}}" alt="Santa Icon" class="iconData">
+                <div class="chart">
+                    <p>Test</p>
+                </div>
+            </div>
+
+            <div class="containerData">
+                <img src="{{asset('img/buttons/faceElf.png')}}" alt="Elf Icon" class="iconData">
+                <div class="chart">
+                    <p>Test</p>
+                </div>
+            </div>
+        </section>
+
         <section class="products">
-            <h3>Our Top Toys</h3>
-            <div class="product-grid">
-                <div class="product-card">Toy 1</div>
-                <div class="product-card">Toy 2</div>
-                <div class="product-card">Toy 3</div>
+            <h3>TOP TOYS</h3>
+            <div class="productsGrid">
+                <div class="productTower">
+                    <p>Second Toy</p>
+                    <img src="{{asset("img/buttons/secondTower.png")}}" alt="Second Tower">
+                </div>
+                <div class="productTower">
+                    <p>First Toy</p>
+                    <img src="{{asset("img/buttons/firstTower.png")}}" alt="First Tower">
+                </div>
+                <div class="productTower">
+                    <p>Third Toy</p>
+                    <img src="{{asset("img/buttons/thirdTower.png")}}" alt="Third Tower">
+                </div>
             </div>
         </section>
 @endsection
