b22ceb0646200ef75edc8d2bcfbadd511ceccb38
Inma Gonzalez <inmagonzalez045@gmail.com>
2025-01-09 14:09:27 +0100

    fix: add colour to behaviour field

--- resources/views/showKid.blade.php
+++ resources/views/showKid.blade.php
@@ -1,31 +1,28 @@
 @extends('layouts.app')
 @section('content')
-
     <div class="containerShow">
 
         <div class="backBtnStyle">
             <a href="{{ route('kidshome') }}" class="backBtnStyle">Back</a>
         </div>
-    
+
         <table class="styleTableShow">
-                <tr> 
-                    <td rowspan="3" class="no-border"><img src="{{$kids->photo}}" alt="{{$kids->name}}"></td>
-                    <td class="nameSurnameTable" colspan="2">{{$kids->name}} {{$kids->surname}}</td>
-                </tr>   
-                <tr>
-                    <td>{{$kids->age}} years old</td>
-                    <td>{{$kids->country}}</td>
-                </tr>
-                <tr>
-                    <td>ID - {{$kids->id}}</td>
-                    <td>
-                        @if ($kids->behaviour === 1)
-                            <span class="active">Good</span>
-                        @else
-                            <span class="inactive">Bad</span>
-                        @endif
-                    </td>
-                </tr>
+            <tr>
+                <td rowspan="3" class="no-border"><img src="{{ $kids->photo }}" alt="{{ $kids->name }}"></td>
+                <td class="nameSurnameTable" colspan="2">{{ $kids->name }} {{ $kids->surname }}</td>
+            </tr>
+            <tr>
+                <td>{{ $kids->age }} years old</td>
+                <td>{{ $kids->country }}</td>
+            </tr>
+            <tr>
+                <td>ID - {{ $kids->id }}</td>
+                @if ($kids->behaviour === 1)
+                    <td class="active">Good</td>
+                @else
+                    <td class="inactive">Bad</td>
+                @endif
+            </tr>
         </table>
     </div>
 @endsection
