0d4be77d910d12b613dc724cdec2f92d917ac460
angiehelensanchez <angiehelensanchez@gmail.com>
2025-01-14 11:17:38 +0100

    feat: add js feature into header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,8 @@
 <header>
     <h1>SANTA'S FACTORY</h1>
-    <div class="headerButtons">
-        <a href="{{ route('kidshome') }}"><img src="{{ asset('img/buttons/buttonSanta.png')}}" alt=""></a>
-        <a href="{{ route('toyshome') }}"><img src="{{asset('img/buttons/buttonElf.png')}}" alt=""></a>
-    </div>
+    <nav class="navbar">
+        <a href="#">Gifts</a></li>
+        <a href="{{ route('kidshome') }}">Santa</a>
+        <a href="{{ route('toyshome') }}">Elfs</a>
+    </nav>
 </header>
