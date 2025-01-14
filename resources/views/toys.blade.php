09d82f0cb3bef584057202841f70ea0d510a4e37
TOKIO-V2 <albertohernandezgaleote6@gmail.com>
2025-01-14 15:22:14 +0100

    fix: Fixed some issues with toy page code.

--- resources/views/toys.blade.php
+++ resources/views/toys.blade.php
@@ -5,34 +5,47 @@
         <a href="{{ route('santashome') }}" class="backBtn">🔙</a>
 </div>
 
-<div class="tableToy">
+<div class="tableDiv">
 <h2 class="form-title">Toys</h2>
-    <table class="table">
+    <table class="tableToy">
         <thead>
             <tr>
-                <th>ID</th>
-                <th>Name</th>
                 <th>Photo</th>
+                <th>Name</th>
+                <th>Minimun age</th>
                 <th>Description</th>
-                <th>Minimun Age</th>
-                <th>Created at</th>
-                <th>Actions</th>
+                <th>Show</th>
+                <th>Edit</th>
+                <th>Delete</th>
             </tr>
         </thead>
         <tb>
             @foreach ($toys as $toy)
                 <tr>
-                    <td>{{$toy->id}}</td>
-                    <td>{{$toy->name}}</td>
                     <td><img src="{{$toy->photo}}" alt="{{$toy->name}}"></td>
-                    <td>{{$toy->description}}</td>
+                    <td>{{$toy->name}}</td>
                     <td>+{{$toy->min_age}}</td>
-                    <td>{{$toy->created_at->format('d/m/y')}}</td>
+                    <td>{{$toy->description}}</td>
                     <td>
+                        <button class="actionButton">
+                            <img src="{{asset('img/buttons/showKid.png')}}" alt="Show Kid Button">
+                        </button>
+                    </td>
+                    <td>
+                        <button class="actionButton">
+                             <img src="{{asset('img/buttons/editKid.png')}}" alt="Edit Kid Button">
+                        </button>
+                    </td>
+                    <td>
+                        <button class="actionButton">
+                            <img src="{{asset('img/buttons/deleteKid.png')}}" alt="Delete Kid Button">
+                        </button>
+                    </td>
+                    {{-- <td>
                         <a class="crudBtn" href="toys/show/{{$toy->id}}">👀</a>
                         <a class="crudBtn" href="toys/create">🆕</a>
                         <a class="crudBtn"href="toys/edit/{{$toy->id}}">📝</a>
-                        <a class="crudBtn" href="?action=delete&id={{$toy->id}}">🗑️</a></td>
+                        <a class="crudBtn" href="?action=delete&id={{$toy->id}}">🗑️</a></td> --}}
                 </tr>
             @endforeach
         <tb>
