8492a21899f2904b7a2a4e7c07d30051fc831de8
Camposx <alejandrocamposgon@gmail.com>
2025-01-14 15:26:21 +0100

    feat: add random function for toys

---
+++ app/Http/Controllers/Api/ToyKidController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Log;
+use App\Models\Kid;
+use App\Models\Toy;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\KidController;
+
+
+class ToyKidController extends Controller
+{
+    public function randomToy(){
+        $toys = Toy::all();       
+        $randomToy = $toys->random();
+        return $randomToy;
+    }
+
+   
+}
