b8ceb88c1016d1e44c700d225607fcbbbecff0ea
TOKIO-V2 <albertohernandezgaleote6@gmail.com>
2025-01-20 13:38:20 +0100

    fix: Toyshow

--- resources/views/showToy.blade.php
+++ resources/views/showToy.blade.php
@@ -4,29 +4,20 @@
         <div class="backButtonDiv">
             <a href="{{ route('toyshome') }}" class="backBtnStyle">Back</a>
         </div>
-
-    <div class="tableToy">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Photo</th>
-                <th>Description</th>
-                <th>Minimun Age</th>
-                <th>Created at</th>
-            </tr>
-            </thead>
-            <tb>
-                <tr>
-                    <td>{{$toys->id}}</td>
-                    <td>{{$toys->name}}</td>
-                    <td><img src="{{$toys->photo}}" alt="{{$toys->name}}"></td>
-                    <td>{{$toys->description}}</td>
-                    <td>+{{$toys->min_age}}</td>
-                    <td>{{$toys->created_at->format('d/m/y')}}</td>
-                </tr>
-            <tb>
-        </table>
+        <div class="containerToys">
+            <table class="styleTableShowToys">
+                <tbody>
+                    <tr>
+                        <td rowspan="3" class="no-border"><img src="{{ $toys->photo }}" alt="{{ $toys->name }}"></td>
+                        <td rowspan="3" class="no-border"></td>
+                        <td class="nameTable">{{ $toys->name }}</td>
+                        <td class="toysAge">+{{ $toys->age_range }}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">{{ $toys->description }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
