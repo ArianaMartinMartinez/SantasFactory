bd8ae346ce4d122d3b6e048404769e525d53470f
angiehelensanchez <angiehelensanchez@gmail.com>
2025-01-20 13:08:28 +0100

    feat: add gifs route

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,7 +5,7 @@
             <li>
                 <a href="{{route('santashome')}}">Home</a></li>
             </li>
-            <li><a href="#">Gifts</a></li></li>
+            <li><a href="{{route('kidsWithToys')}}">Gifts</a></li></li>
             <li><a href="{{ route('kidshome') }}">Santa</a></li>
             <li><a href="{{ route('toyshome') }}">Elfs</a></li>
 
