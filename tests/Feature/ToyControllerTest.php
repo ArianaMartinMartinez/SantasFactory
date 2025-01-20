4394f7d0623b7b7c1a4756e92724e0b8a8962c9b
Camposx <alejandrocamposgon@gmail.com>
2025-01-20 15:04:12 +0100

    feat: Add test for every ToyController Web functions

--- tests/Feature/ToyControllerTest.php
+++ tests/Feature/ToyControllerTest.php
@@ -2,19 +2,92 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Toy;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Kid;
 
 class ToyControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+    public function test_ListOfEntryCanBeRead()
     {
-        $response = $this->get('/');
+        $this->withoutExceptionHandling();
+        Toy::all();
 
-        $response->assertStatus(200);
+        $response = $this->get('/elf/toys');
+        $response->assertStatus(200)
+            ->assertViewIs('toys');
     }
+
+    public function test_CreateFunctionReturnViewCorrectly()
+    {
+
+        $response = $this->get('/elf/toys/create');
+
+        $response->assertStatus(200)
+            ->assertViewIs('createToyForm');
+    }
+
+    public function test_storeMethodSavesObjectCorrectly()
+    {
+        $toy = [
+            'name' => 'test',
+            'description' => 'test',
+            'photo' => 'test',
+            'age_range' => '0-3'
+        ];
+
+        $response = $this->post(route('toysstore'), $toy);
+
+        $response = $this->get(route('toyshome'));
+        $response->assertStatus(200);
+        $this->assertDatabaseCount('Toys', 1);
+    }
+
+    public function test_checkIfShowViewWorksCorrectly()
+    {
+        $toy = Toy::factory()->create();
+
+        $response = $this->get(route('toysshow', $toy->id));
+
+        $response->assertStatus(200)
+            ->assertViewIs('showToy');
+    }
+
+    public function test_checkIfEditFormViewWorksCorrectly()
+    {
+        $toy = Toy::factory()->create();
+
+        $response = $this->get(route('toysedit', $toy->id));
+
+        $response->assertStatus(200)
+            ->assertViewIs('editToyForm');
+    }
+
+    public function test_checkIfUpdateMethodWorksCorrectly()
+    {
+        $toy = Toy::factory()->create();
+
+        $response = $this->put(route('toysupdate', 1), 
+        [
+            'name' => 'updatedToy',
+            'description' => $toy->description,
+            'photo' => $toy->photo,
+            'age_range' => $toy->age_range
+        ]);
+
+        $toy = Toy::find(1);
+        $this->assertEquals('updatedToy', $toy->name);
+    }
+
+    public function test_checkIfDeleteMethodDestroyElement()
+    {
+        Toy::factory(1)->create();
+
+        $response = $this->delete(route('toysdestroy',1));
+        $this->assertDatabaseCount('Toys', 0);
+    }
+
 }
