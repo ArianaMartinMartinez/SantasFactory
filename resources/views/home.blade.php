6fa6ecc105f764b37d6a4ae889e9f46543ddf0d5
Jaz356 <joziahwalters70@gmail.com>
2025-01-21 15:37:52 +0100

    fix/home spacing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,14 +26,14 @@
                     <div id="chart2"></div>
                     <div class="leyendChart">
                         <ul class="right-list">
-                            <li id="data0To3">0-3:{{$allToysData[0] + 3}}</li>
-                            <li id="data3To7">3-7:{{$allToysData[1] + 3}}</li>
-                            <li id="data7To12">7-12:{{$allToysData[2] + 3}}</li>
-                            <li id="data12To16">12-16:{{$allToysData[3] + 3}}</li>
-                            <li id="data16To18">16-18:{{$allToysData[4] + 3}}</li>
-                            <li id="dataMore18">+18:{{$allToysData[5]}}</li>
+                            <li id="data0To3">0-3 : {{$allToysData[0] + 3}}</li>
+                            <li id="data3To7">3-7 : {{$allToysData[1] + 3}}</li>
+                            <li id="data7To12">7-12 : {{$allToysData[2] + 3}}</li>
+                            <li id="data12To16">12-16 : {{$allToysData[3] + 3}}</li>
+                            <li id="data16To18">16-18 : {{$allToysData[4] + 3}}</li>
+                            <li id="dataMore18">+18 : {{$allToysData[5] }}</li>
                         </ul>
-                        <p>Total:{{$allToysData[0] + $allToysData[1] + $allToysData[2] + $allToysData[3] + $allToysData[4] + $allToysData[5] + 3}} </p>
+                        <p>Total: {{$allToysData[0] + $allToysData[1] + $allToysData[2] + $allToysData[3] + $allToysData[4] + $allToysData[5] + 3}} </p>
                     </div>
                 </div>
             </div>
